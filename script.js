e80b6bb60cc09a4b0bba8fdf64e5b681df0bd313
Erick Cunha <erickcuna20@gmail.com>
2025-03-19 08:26:15 -0300

    feat: Criando timer que passa os sliders automaticamente

--- script.js
+++ script.js
@@ -24,3 +24,5 @@ function updateMargin() {
     let newMargin = (currentSlide * document.body.clientWidth);
     document.querySelector('.slider--width').style.marginLeft=`${-newMargin}px`;
 }
+
+setInterval(goNext, 4000);
